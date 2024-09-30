2254b1e1a95d80d488908b5ae8b6e258091893c9
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-09-30 13:38:11 +0700

    ex-1and2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
-//Exercise #1: Variable Creation
-//Start coding here
+// Exercise #1: Variable Creation
+// Start coding here
+let name = "Karter";
+let surName = "Anderson";
+let location = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+const restaurantName = "Chef's Table by Chef James";
+const foodPrice = 15000;
+const allRestaurantsOpen = 1500;
+
+location = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”";
+foodPrice = 5000;
+
+allRestaurantsOpen = 2500;
+
+console.log(location);
