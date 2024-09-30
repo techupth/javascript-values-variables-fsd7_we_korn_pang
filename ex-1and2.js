d4eb81e874c5fa293224ccd2f9e3f7399c8fa4c6
pangphaichit <pg_grj@hotmail.com>
2024-09-30 13:54:19 +0700

    Assignment-1

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstname = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const netFoodPrice = 15000;
+const numberOfOpenRestaurants = 1500;
+
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netFoodPrice = 5000;
+numberOfOpenRestaurants = 2500;
+
