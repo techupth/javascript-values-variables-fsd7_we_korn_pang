95fe9fb8fe4995669ce246cb8fdb9a81fe0afd91
Natthanat Chuayriang <natthanataa@gmail.com>
2024-09-30 13:30:29 +0700

    feat: finish ex_1&2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,22 @@
 //Exercise #1: Variable Creation
 //Start coding here
+const firstName = "Karter";
+const lastName = "Anderson";
+const address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+let nameRestaurant = "Chef's Table by Chef Jame"
+let totalSale = 15000;
+let currentOpening = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalSale = 5000;
+currentOpening = 2500;
+
+console.log(address);
+console.log(totalSale);
+console.log(currentOpening);
+
+
